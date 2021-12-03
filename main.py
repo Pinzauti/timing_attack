7ccddabffcb9469ba0b2a484c03833288b84d087
Francesco Pinzauti <francescopinzauti@gmail.com>
2021-12-03 15:33:37 +0100

    fix: pylint

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import target
 
 def stats(length):
     """
-    This is necessary in order to plot the graph. We are generating random password to guess and then trying to guess
-    them.
+    This is necessary in order to plot the graph. We are generating random password to
+    guess and then trying to guess them.
     :param length: The actual length of c, that then we will try to guess.
     :return: It returns the time that was necessary to crack the password.
     """
     target.c = ''.join(random.choices(string.ascii_lowercase, k=length))
-    print('The password of length %d to guess is %s' % (int(length), target.c))
+    print(f'The password of length {int(length)} to guess is {target.c}')
     result = attack.attack(attack.guess_length())[0]
     time = attack.attack(attack.guess_length())[1]
-    print('The guessed password of length %d is %s and it required %d nanoseconds to crack it' % (int(length), result,
-                                                                                                  int(time)))
+    print(f'The guessed password of length {int(length)} is {result} and it required {int(time)}'
+          f'nanoseconds to crack it')
     if target.c == result:
         return time
+    return 0
 
 
 x = [1, 2, 3, 4, 5]
